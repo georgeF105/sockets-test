d5acb700b67e87fe69117fe9a77e2065aa81dccc
george francis <georgefrancis105@gmail.com>
2016-07-20 11:33:26 +1200

    Make basic chat functionality

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', function(socket) {
+	console.log('User connected')
+
+	socket.on('SEND_MSG', function(msg) {
+		console.log('Got msg: ',msg)
+		io.emit('SEND_MSG', msg)
+	})
+
+	socket.on('disconnect', function(){
+		console.log('User disconnected')
+	})
+})
+
 http.listen(3000, function() {
   console.log('listening on localhost:3000')
 })
--- public/sockets.js
+++ public/sockets.js
@@ -2,5 +2,15 @@ var socket = io();
 
 document.querySelector('form').addEventListener('submit', function(e) {
   e.preventDefault();
-  alert("this button doesn't do anything yet!")
+  var val = document.getElementById('message').value
+	socket.emit('SEND_MSG',val)  
+})
+
+socket.on('SEND_MSG', function(msg) {
+	var liMsg = document.createElement("LI")
+	console.log('liMsg',liMsg)
+	liMsg.appendChild(document.createTextNode(msg))
+	// console.log('liMsg',liMsg)
+	document.getElementById('messages').appendChild(liMsg)
+
 })
